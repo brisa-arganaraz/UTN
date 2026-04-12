bf2a054b4e7a86cdbdbd7a9aefe683a6b21d47f4
RamaT17 <ramitrini1997@gmail.com>
2026-04-12 17:24:00 -0300

    Update sprint1.py

--- sprint1.py
+++ sprint1.py
@@ -171,8 +171,64 @@ if Plataforma == "Playstation":
             print("Un juego para alguien menor de 18 años y cumpliendo con las demas especificaciones seria Astro's Playroom")      
 
 
+if Plataforma == "PC":
+    
+if Genero == "Terror" and not Edad >= 16:
+    print(Fore.RED + "Lo sentimos " + Nick + ", los juegos de Terror son para mayores de 16.")
+    print(Fore.YELLOW + "Basado en tu edad, te sugerimos cambiar a Accion o Aventura.")
+    Genero = input(Fore.BLUE + "Escriba el nuevo Género para continuar: ")
 
-# --- FALTA EL DE PC ACA ---
+if Genero == "Accion" and not Edad >= 13:
+    print(Fore.RED + "Lo sentimos " + Nick + ", los juegos de Accion son para mayores de 13.")
+    print(Fore.YELLOW + "Te sugerimos cambiar al genero Aventura.")
+    Genero = input(Fore.BLUE + "Escriba el nuevo genero para continuar: ")
+
+while Almacenamiento < 100:
+    print(Fore.RED + "Error: Espacio insuficiente.")
+    Almacenamiento = int(input(Fore.WHITE + "Reingrese almacenamiento (minimo 100GB): "))
+
+while Presupuesto < 2000:
+    print(Fore.RED + "Error: Presupuesto insuficiente.")
+    Presupuesto = int(input(Fore.WHITE + "Reingrese presupuesto (minimo 2000): "))
+
+if Presupuesto > 2000:
+    Tipo_Presupuesto = "Bajo"
+if Presupuesto > 5000:
+    Tipo_Presupuesto = "Medio"
+if Presupuesto > 8000:
+    Tipo_Presupuesto = "Alto"
+    
+    if Genero == "Aventura":
+        if Jugadores == 1 and Tipo_Presupuesto == "Bajo" and Edad >= 10:
+            print(Fore.YELLOW + "Recomendacion: Hollow Knight.")
+            if Presupuesto > 5000:
+                print(Fore.CYAN + "Tip: Tambien tenes presupuesto para It Takes Two.")
+        
+        if Jugadores >= 2 and Tipo_Presupuesto == "Medio":
+            print(Fore.YELLOW + "Recomendacion: It Takes Two.")
+            
+        if Almacenamiento >= 100 and Tipo_Presupuesto == "Alto" and Edad >= 18:
+            print(Fore.YELLOW + "Recomendacion: Grand Theft Auto V.")
+
+    if Genero == "Accion":
+        if Jugadores >= 1 and Tipo_Presupuesto == "Bajo" and Edad >= 13:
+            print(Fore.YELLOW + "Recomendacion: Counter-Strike 2.")
+            if Presupuesto > 8000:
+                 print(Fore.CYAN + "Tip: Como tenes buen presupuesto, podrias comprar Cyberpunk 2077.")
+
+        if Jugadores == 1 and Edad >= 18 and Tipo_Presupuesto == "Alto":
+            print(Fore.YELLOW + "Recomendacion: Cyberpunk 2077.")
+
+    if Genero == "Terror" and Edad >= 16:
+        if Jugadores == 1 and Tipo_Presupuesto == "Bajo":
+            print(Fore.YELLOW + "Recomendacion: Outlast.")
+            if Presupuesto > 4000:
+                print(Fore.CYAN + "Tip: Tambien podes sumar el juego Phasmophobia para jugar con amigos.")
+
+        if Jugadores >= 2 and Tipo_Presupuesto == "Medio":
+            print(Fore.YELLOW + "Recomendacion: Phasmophobia.")
+
+print(Fore.GREEN + "Gracias por usar el sistema de recomendaciones, " + Nick + "!")
 
 
 
